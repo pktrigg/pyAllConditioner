016458da08ee1f351e9f513da8f84d866fcff21a
pktrigg <pktrigg@gmail.com>
2018-05-09 16:21:28 +0800

    updates for AIMS review

--- pyallconditioner.py
+++ pyallconditioner.py
@@ -8,6 +8,7 @@
 import csv
 import sys
 import time
+import math
 import os
 import fnmatch
 from argparse import ArgumentParser
@@ -21,6 +22,7 @@ import struct
 import sortedcollection
 from operator import itemgetter
 from collections import deque
+from collections import defaultdict
 
 ###############################################################################
 def main():
@@ -34,6 +36,9 @@ def main():
 	parser.add_argument('-extractbs', action='store_true', default=False, dest='extractbs', help='Extract backscatter from Y datagram so we can analyse. [Default: False]')
 	parser.add_argument('-r', action='store_true', default=False, dest='recursive', help='Search Recursively from the current folder.  [Default: False]')
 	parser.add_argument('-svp', action='store_true', default=False, dest='svp', help='Output a CARIS compatible SVP file based on the sound velocity datagram.  [Default: False]')
+	parser.add_argument('-attitude', action='store_true', default=False, dest='attitude', help='Output a CSV file of attitude.  [Default: False]')
+	parser.add_argument('-nadir', action='store_true', default=False, dest='nadir', help='Output a CSV file of the nadir beam.  [Default: False]')
+	parser.add_argument('-install', action='store_true', default=False, dest='install', help='Output the installation parameters to a CSV. [Default: False]')
 	parser.add_argument('-bscorr', action='store_true', default=False, dest='bscorr', help='Output the backscatter bscorr.txt file as used in the PU.  This is useful for backscatter calibration and processing, and removes the need to telnet into the PU.   [Default: False]')
 	parser.add_argument('-splitd', action='store_true', default=False, dest='splitd', help='split the .all file every time the depth mode changes.  [Default: False]')
 	parser.add_argument('-splitt', dest='splitt', action='store', default="", help='Split the .all file based on time inm secnds e.g. -splitt 60')
@@ -47,6 +52,8 @@ def main():
 	fileCounter=0
 	matches = []
 	inject = False
+	extractAttitude=False
+	extractNadir=False
 	extractBackscatter = False
 	correctBackscatter = False
 	writeConditionedFile = True
@@ -54,6 +61,7 @@ def main():
 	extractSVP = False
 	extractBSCorr = False
 	splitd=False
+	install=False
 	latitude = 0
 	longitude = 0
 	splitt = 0
@@ -63,14 +71,14 @@ def main():
 		for root, dirnames, filenames in os.walk(os.path.dirname(args.inputFile)):
 			for f in fnmatch.filter(filenames, '*.all'):
 				matches.append(os.path.join(root, f))
-				print (matches[-1])
+				# print (matches[-1])
 	else:
 		if os.path.exists(args.inputFile):
 			matches.append (os.path.abspath(args.inputFile))
 		else:
 			for filename in glob(args.inputFile):
 				matches.append(filename)
-		print (matches)
+		# print (matches)
 
 	if len(matches) == 0:
 		print ("Nothing found in %s to condition, quitting" % args.inputFile)
@@ -113,6 +121,27 @@ def main():
 		# auto exclude attitude records.  on reflection, we should probably NOT do this.
 		# args.exclude = 'n'
 
+	if args.install:
+		install=True
+		writeConditionedFile= False #we do not need to write out a .all file
+		r = pyall.ALLReader(matches[0])
+		installStart, installStop, initialMode, datagram = r.loadInstallationRecords()
+		r.close()
+		# header = "Waterline(m),Transmit X(m), Transmit Y(m), Transmit Z(m), Receive(X), Receive(Y), Receive(Z), "
+		# print(header)
+		header = "Filename"
+		for code in datagram.installationParameters:
+			header = header + "," + InstallationCodeToText(code) + " (" + code + ")"
+		print (header)
+
+	if args.attitude:
+		extractAttitude=True
+		writeConditionedFile= False #we do not need to write out a .all file
+
+	if args.nadir:
+		extractNadir=True
+		writeConditionedFile= False #we do not need to write out a .all file
+		
 	if args.bscorr:
 		extractBSCorr=True
 		writeConditionedFile= False #we do not need to write out a .all file
@@ -126,6 +155,14 @@ def main():
 		initialDepthMode = ""
 
 	for filename in matches:
+		if extractAttitude:
+			# create an output file based on the input
+			outFileName = os.path.join(os.path.dirname(os.path.abspath(filename)), args.odir, os.path.basename(filename))
+			outFileName = os.path.splitext(outFileName)[0]+'.txt'
+			outFileName  = createOutputFileName(outFileName,".txt")
+			outFilePtr = open(outFileName, 'w')
+			print ("writing to file: %s" % outFileName)
+
 		if writeConditionedFile:
 			# create an output file based on the input
 			outFileName = os.path.join(os.path.dirname(os.path.abspath(filename)), args.odir, os.path.basename(filename))
@@ -133,8 +170,18 @@ def main():
 			outFilePtr = open(outFileName, 'wb')
 			print ("writing to file: %s" % outFileName)
 
+		# open the file and do some initialisation stuff 
 		r = pyall.ALLReader(filename)
 		counter = 0
+
+		if extractNadir:
+			print ("NadirDepth,TxDepth, Pitch,Heave,Roll ")
+		
+		if extractAttitude:
+			# read the first record so we get a date for the file header
+			typeOfDatagram, datagram = r.readDatagram()
+			str = r.currentRecordDateTime().strftime('%Y%m%d')
+			outFilePtr.write("Name:" + os.path.basename(filename) + "," + str + "\n")
 
 		if splitd:
 			InstallStart, InstallEnd, initialDepthMode = r.loadInstallationRecords()
@@ -149,11 +196,15 @@ def main():
 			r.rewind()
 
 		if extractSVP:
-			# we need the position of teh SVP dip in the SVP file, so use thge first position record in the file
+			# we need the position of the SVP dip in the SVP file, so use the first position record in the file
 			nav = r.loadNavigation(True)
 			if len(nav) > 0:
 				latitude = nav[0][1]
 				longitude = nav[0][2]
+
+		currPitch = 0
+		currRoll = 0
+		currHeave = 0
 
 		while r.moreData():
 			# read a datagram.  If we support it, return the datagram type and aclass for that datagram
@@ -165,6 +216,40 @@ def main():
 			# read the bytes into a buffer 
 			rawBytes = r.readDatagramBytes(datagram.offset, datagram.numberOfBytes)
 
+			if extractNadir:
+				if TypeOfDatagram == 'D' or  TypeOfDatagram == 'X':
+					datagram.read()
+					# find the depth nearest to Nadir
+					# https://stackoverflow.com/questions/9706041/finding-index-of-an-item-closest-to-the-value-in-a-list-thats-not-entirely-sort
+					nadirBeam = min(range(len(datagram.AcrossTrackDistance)), key=lambda i: abs(datagram.AcrossTrackDistance[i]))
+					print ("%.3f,%.3f,%.3f,%.3f,%.3f" % (datagram.Depth[nadirBeam], datagram.TransducerDepth/100, currRoll, currPitch, currHeave))
+				if TypeOfDatagram == 'A':
+					datagram.read()
+					currRoll = datagram.Attitude[-1][3]
+					currPitch = datagram.Attitude[-1][4]
+					currHeave = datagram.Attitude[-1][5]
+			if extractAttitude:
+				if TypeOfDatagram == 'A':
+					datagram.read()
+					for a in datagram.Attitude:
+						dateobject = pyall.to_DateTime(a[0], a[1])
+						str = ("%d,%.3f,%.3f,%.3f,%.3f\n" % (a[0],a[1],a[3],a[4],a[5]))
+						outFilePtr.write(str)
+						# outFilePtr.write("%s,%.3f" % (pyall.to_timestamp(dateobject), a[5]))
+					currRoll = datagram.Attitude[len(datagram.Attitude)[3]]
+					currPitch = datagram.Attitude[len(datagram.Attitude)[4]]
+					currHeave = datagram.Attitude[len(datagram.Attitude)[5]]
+			if install:
+				if TypeOfDatagram == 'I':
+					datagram.read()
+					row = filename
+					for i in datagram.installationParameters :
+						if len(datagram.installationParameters[i]) == 0:
+							datagram.installationParameters[i] = "0.00"
+						row = row + "," + datagram.installationParameters[i]
+						# row.replace(",,",",")
+					print (row)
+					break
 			if splitt:
 					# datagram.read()
 					if pyall.to_timestamp(r.currentRecordDateTime()) > (splitfileend + splitt):
@@ -258,7 +343,7 @@ def main():
 			if writeConditionedFile:
 				outFilePtr.write(rawBytes)
 
-		update_progress("Processed: %s (%d/%d)" % (filename, fileCounter, len(matches)), (fileCounter/len(matches)))
+		# update_progress("Processed: %s (%d/%d)" % (filename, fileCounter, len(matches)), (fileCounter/len(matches)))
 		fileCounter +=1
 		r.close()
 
@@ -283,11 +368,142 @@ def main():
 					beamARC = (beam.sampleSum/beam.numberOfSamplesPerBeam)
 					f.write("%.3f, %.3f, %d, %d, %d, %.3f\n" % (beam.takeOffAngle, beamARC, beam.sector, beam.sampleSum, beam.numberOfSamplesPerBeam , beamARC + responseAverage))
 
-	update_progress("Process Complete: ", (fileCounter/len(matches)))
+	# update_progress("Process Complete: ", (fileCounter/len(matches)))
 	if writeConditionedFile:
 		print ("Saving conditioned file to: %s" % outFileName)		
 		outFilePtr.close()
 
+
+def InstallationCodeToText(code):
+
+	allcodes = defaultdict(str)
+
+	allcodes['WLZ'] = 'Water line vertical location in m'
+	allcodes['SMH'] = 'System main head serial number'
+	allcodes['HUN'] = 'Hull Unit'
+	allcodes['HUT'] = 'Hull Unit tilt offset'
+	allcodes['TXS'] = 'TX serial number'
+	allcodes['T2X'] = 'TX no. 2 serial number'
+	allcodes['R1S'] = 'RX no. 1 serial number'
+	allcodes['R2S'] = 'RX no. 2 serial number'
+	allcodes['STC'] = 'System transducer configuration'
+	allcodes['S0Z'] = 'Transducer 0 vertical location in m'
+	allcodes['S0X'] = 'Transducer 0 along location in m'
+	allcodes['S0Y'] = 'Transducer 0 athwart location in m'
+	allcodes['S0H'] = 'Transducer 0 heading in degrees'
+	allcodes['S0R'] = 'Transducer 0 roll in degrees re horizontal'
+	allcodes['S0P'] = 'Transducer 0 pitch in degrees'
+	allcodes['S1Z'] = 'Transducer 1 vertical location in m'
+	allcodes['S1X'] = 'Transducer 1 along location in m'
+	allcodes['S1Y'] = 'Transducer 1 athwart location in m'
+	allcodes['S1H'] = 'Transducer 1 heading in degrees'
+	allcodes['S1R'] = 'Transducer 1 roll in degrees re horizontal'
+	allcodes['S1P'] = 'Transducer 1 pitch in degrees'
+	allcodes['S1N'] = 'Transducer 1 no of modules'
+	allcodes['S2Z'] = 'Transducer 2 vertical location in m'
+	allcodes['S2X'] = 'Transducer 2 along location in m'
+	allcodes['S2Y'] = 'Transducer 2 athwart location in m'
+	allcodes['S2H'] = 'Transducer 2 heading in degrees'
+	allcodes['S2R'] = 'Transducer 2 roll in degrees re horizontal'
+	allcodes['S2P'] = 'Transducer 2 pitch in degrees'
+	allcodes['S2N'] = 'Transducer 2 no of modules'
+	allcodes['S2Z'] = 'Transducer 3 vertical location in m'
+	allcodes['S3Z'] = 'Transducer 3 along location in m'
+	allcodes['S3Y'] = 'Transducer 3 athwart location in m'
+	allcodes['S2H'] = 'Transducer 3 heading in degrees'
+	allcodes['S3R'] = 'Transducer 3 roll in degrees re horizontal'
+	allcodes['S3P'] = 'Transducer 3 pitch in degrees'
+	allcodes['S1S'] = 'TX array size (0=0.5 1=1 2=2)'
+	allcodes['S2S'] = 'RX array size (1=1 2=2)'
+	allcodes['GO1'] = 'System (sonar head 1) gain offset'
+	allcodes['GO2'] = 'Sonar head 2 gain offset'
+	allcodes['OBO'] = 'Outer beam offset'
+	allcodes['FGD'] = 'High/Low Frequency Gain Difference'
+	allcodes['TSV'] = 'Transmitter (sonar head no1) software version'
+	allcodes['RSV'] = 'Receiver (sonar head 2) software version'
+	allcodes['BSV'] = 'BSP software version'
+	allcodes['PSV'] = 'Processing unit software version'
+	allcodes['DDS'] = 'DDS software version'
+	allcodes['OSV'] = 'Operator station software version'
+	allcodes['DSV'] = 'Datagram format version'
+	allcodes['DSX'] = 'Depth (pressure) sensor along location in m'
+	allcodes['DSY'] = 'Depth (pressure) sensor athwart location in m'
+	allcodes['DSZ'] = 'Depth (pressure) sensor vertical location in m'
+	allcodes['DSD'] = 'Depth (pressure) sensor time delay in millisec'
+	allcodes['DSO'] = 'Depth (pressure) sensor offset'
+	allcodes['DSF'] = 'Depth (pressure) sensor scale factor'
+	allcodes['DSH'] = 'Depth (pressure) sensor heave'
+	allcodes['APS'] = 'Active position system number'
+	allcodes['P1Q'] = 'Position system 1 quality check of position 0:off 1:on'
+	allcodes['P1M'] = 'Position system 1 motion compensation'
+	allcodes['P1T'] = 'Position system 1 time stamp used'
+	allcodes['P1Z'] = 'Position system 1 vertical location in m'
+	allcodes['P1X'] = 'Position system 1 along location in m'
+	allcodes['P1Y'] = 'Position system 1 athwart location in m'
+	allcodes['P1D'] = 'Position system 1 time delay in seconds'
+	allcodes['P1G'] = 'Position system 1 geodetic datum'
+	allcodes['P2Q'] = 'Position system 2 quality check of position 0:off 1:on'
+	allcodes['P2M'] = 'Position system 2 motion compensation'
+	allcodes['P2T'] = 'Position system 2 time stamp use'
+	allcodes['P2Z'] = 'Position system 2 vertical location in m'
+	allcodes['P2X'] = 'Position system 2 along location in m'
+	allcodes['P2Y'] = 'Position system 2 athwart location in m'
+	allcodes['P2D'] = 'Position system 2 time delay in seconds'
+	allcodes['P2G'] = 'Position system 2 geodetic datum'
+	allcodes['P3Q'] = 'Position system 3 quality check of position 0:off 1:on'
+	allcodes['P3M'] = 'Position system 3 motion compensation'
+	allcodes['P3T'] = 'Position system 3 time stamp use'
+	allcodes['P3Z'] = 'Position system 3 vertical location in m'
+	allcodes['P3X'] = 'Position system 3 along location in m'
+	allcodes['P3Y'] = 'Position system 3 athwart location in m'
+	allcodes['P3D'] = 'Position system 3 time delay in seconds'
+	allcodes['P3G'] = 'Position system 3 geodetic datum'
+	allcodes['P3S'] = 'Position system 3 on serial line or Ethernet'
+	allcodes['MSZ'] = 'Motion sensor 1 vertical location in m'
+	allcodes['MSX'] = 'Motion sensor 1 along location in m'
+	allcodes['MSY'] = 'Motion sensor 1 athwart location in m'
+	allcodes['MRP'] = 'Motion sensor 1 roll reference plane'
+	allcodes['MSD'] = 'Motion sensor 1 time delay in milliseconds'
+	allcodes['MSR'] = 'Motion sensor 1 roll offset in degrees'
+	allcodes['MSP'] = 'Motion sensor 1 pitch offset in degrees'
+	allcodes['MSG'] = 'Motion sensor 1 heading offset in degrees'
+	allcodes['NSZ'] = 'Motion sensor 2 vertical location in m'
+	allcodes['NSX'] = 'Motion sensor 2 along location in m'
+	allcodes['NSY'] = 'Motion sensor 2 athwart location in m'
+	allcodes['NRP'] = 'Motion sensor 2 roll reference plane'
+	allcodes['NSD'] = 'Motion sensor 2 time delay in milliseconds'
+	allcodes['NSR'] = 'Motion sensor 2 roll offset in degrees'
+	allcodes['NSP'] = 'Motion sensor 2 pitch offset in degrees'
+	allcodes['NSG'] = 'Motion sensor 2 heading offset in degrees'
+	allcodes['GCG'] = 'Gyrocompass heading offset in degrees'
+	allcodes['MAS'] = 'Roll scaling factor'
+	allcodes['SHC'] = 'Transducer depth sound speed source'
+	allcodes['PPS'] = '1PPS clock synchronization'
+	allcodes['CLS'] = 'Clock source'
+	allcodes['CLO'] = 'Clock offset in seconds'
+	allcodes['VSN'] = 'Active attitude velocity sensor'
+	allcodes['VSU'] = 'Attitude velocity sensor 1 UDP port address (UDP5)'
+	allcodes['VSE'] = 'Attitude velocity sensor 1 Ethernet port'
+	allcodes['VTU'] = 'Attitude velocity sensor 2 UDP port address (UDP6)'
+	allcodes['VTE'] = 'Attitude velocity sensor 2 Ethernet port'
+	allcodes['ARO'] = 'Active roll/pitch sensor'
+	allcodes['AHE'] = 'Active heave sensor'
+	allcodes['AHS'] = 'Active heading sensor'
+	allcodes['VSI'] = 'Ethernet 2 address'
+	allcodes['VSM'] = 'Ethernet 2 IP network mask'
+	allcodes['MCA'] = 'Multicast sensor IP multicast address (Ethernet 2)'
+	allcodes['MCU'] = 'Multicast sensor UDP port number'
+	allcodes['MCI'] = 'Multicast sensor identifier'
+	allcodes['MCP'] = 'Multicast position system number'
+	allcodes['SNL'] = 'Ships noise level'
+	allcodes['CPR'] = 'Cartographic projection'
+	allcodes['ROP'] = 'Responsible operatr'
+	allcodes['SID'] = 'Survey identifier'
+	allcodes['RFN'] = 'Raw File Name'
+	allcodes['PLL'] = 'Survey line identifier (planned line no)'
+	allcodes['COM'] = 'Comment'
+
+	return allcodes[code]
 
 ###############################################################################
 def loadARC(conditionbs):
@@ -429,7 +645,7 @@ def update_progress(job_title, progress):
 	sys.stdout.flush()
 
 ###############################################################################
-def createOutputFileName(path):
+def createOutputFileName(path, ext=""):
 	'''Create a valid output filename. if the name of the file already exists the file name is auto-incremented.'''
 	path = os.path.expanduser(path)
 
@@ -439,7 +655,12 @@ def createOutputFileName(path):
 	if not os.path.exists(path):
 		return path
 
-	root, ext = os.path.splitext(os.path.expanduser(path))
+	if len(ext) == 0:
+		root, ext = os.path.splitext(os.path.expanduser(path))
+	else:
+		# use the user supplied extension
+		root, ext2 = os.path.splitext(os.path.expanduser(path))
+	
 	dir	   = os.path.dirname(root)
 	fname	 = os.path.basename(root)
 	candidate = fname+ext
---
+++ savgoltestp.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from numpy import genfromtxt
+import csv
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
+from glob import glob
+
+def main():
+	parser = ArgumentParser(description='Readatime series CSV of time, heave remove the DC offset from the heave and write the result to a new CSV.',
+			epilog='Example: \n -i c:/temp/myheave.csv \n', formatter_class=RawTextHelpFormatter)
+	parser.add_argument('-i', dest='inputFile', action='store', help='Input csv filename to repair. this can also be a wildcard')
+	parser.add_argument('-odir', dest='odir', action='store', default="", help='Specify a relative output folder e.g. -odir conditioned')
+	parser.add_argument('-level', dest='level', action='store', default="1000", help='Smoothing level (1-5000). [default: 1000')
+
+	if len(sys.argv)==1:
+		parser.print_help()
+		sys.exit(1)
+		
+	args = parser.parse_args()
+	matches = []
+
+	if os.path.exists(args.inputFile):
+		matches.append (os.path.abspath(args.inputFile))
+	else:
+		for filename in glob(args.inputFile):
+			matches.append(filename)
+
+	if len(matches) == 0:
+		print ("Nothing found in %s to condition, quitting" % args.inputFile)
+		exit()
+
+	level = int(args.level)
+
+	for filename in matches:
+		# create an output file based on the input
+		outFileName = os.path.join(os.path.dirname(os.path.abspath(filename)), args.odir, os.path.basename(filename))
+		outFileName  = createOutputFileName(outFileName)
+		outFilePtr = open(outFileName, 'wb')
+		print ("writing to file: %s" % outFileName)
+
+		d = []
+		t = []
+		roll = []
+		pitch = []
+		rawHeave = []
+		with open(filename, 'r') as csvfile:
+				reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+				header = next(reader) 
+				for row in reader:
+					d.append(float(row[0]))
+					t.append(float(row[1]))
+					roll.append(float(row[2]))
+					pitch.append(float(row[3]))
+					rawHeave.append(float(row[4]))
+
+		smoothed = signal.savgol_filter(rawHeave, 101, 1) 
+		for i in range(level):
+			smoothed = signal.savgol_filter(smoothed, 101, 1) 
+
+		diff = np.subtract(rawHeave, smoothed)
+
+		with open(outFileName,'w') as file:
+			for h in header:
+				file.write(h + ",")
+			file.write("\n")
+			for j in range(0, len(d)):
+				# save the corrected data back to a CSV file in the same format as it was read.
+				line = ("%d,%.3f,%.3f,%.3f,%.3f\n" % (d[j], t[j], roll[j], pitch[j], diff[j]))
+				file.write(line)
+
+		plt.figure(figsize=(12,4))
+		plt.axhline(0, color='black', linewidth=0.3)
+		plt.grid(linestyle='-', linewidth='0.2', color='black')
+
+		raw = plt.plot(roll, color='red', linewidth=0.2, label='Raw Heave')
+		error = plt.plot(smoothed, color='black', linewidth=0.2, label='Heave Error')
+		corr = plt.plot(diff, color='blue', linewidth=0.2, label='Corrected Heave')
+
+		plt.legend()
+		plt.xlabel('Sample #')
+		plt.ylabel('Heave(m)')
+		plt.title(os.path.basename(filename))
+		plt.savefig(os.path.splitext(outFileName)[0]+'.png', dpi = 300)
+		# plt.show()
+
+###############################################################################
+def createOutputFileName(path):
+	'''Create a valid output filename. if the name of the file already exists the file name is auto-incremented.'''
+	path = os.path.expanduser(path)
+
+	if not os.path.exists(os.path.dirname(path)):
+		os.makedirs(os.path.dirname(path))
+
+	if not os.path.exists(path):
+		return path
+
+	root, ext = os.path.splitext(os.path.expanduser(path))
+	dir	   = os.path.dirname(root)
+	fname	 = os.path.basename(root)
+	candidate = fname+ext
+	index	 = 1
+	ls		= set(os.listdir(dir))
+	while candidate in ls:
+			candidate = "{}_{}{}".format(fname,index,ext)
+			index	+= 1
+
+	return os.path.join(dir, candidate)
+
+if __name__ == "__main__":
+        main()
